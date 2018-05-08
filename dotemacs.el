1591532a4f2764bd77fcbfec8a671fdd3d23bf34
Duong Dang <duong.dang@hm.com>
2018-05-08 20:12:39 +0000

    Update basic configs.

--- dotemacs.el
+++ dotemacs.el
@@ -8,6 +8,20 @@
 
 (add-to-list 'load-path "~/.emacs.d/lisp")
 
+; activate all the packages (in particular autoloads)
+(package-initialize)
+
+; fetch the list of packages available
+(unless package-archive-contents
+  (package-refresh-contents))
+
+(setq package-list '(auto-complete ensime better-defaults material-theme elpy py-autopep8 color-theme anaconda-mode))
+
+; install the missing packages
+(dolist (package package-list)
+  (unless (package-installed-p package)
+        (package-install package)))
+
 ;; auto-complete stuff
 (require 'auto-complete)
 (require 'auto-complete-config)
@@ -17,9 +31,8 @@
 
 (ido-mode t)
 
-
-(require 'color-theme)
-(load-theme 'wombat)
+;; (require 'color-theme)
+;; (load-theme 'wombat)
 
 (setq
  backup-by-copying t      ; don't clobber symlinks
@@ -69,3 +82,17 @@
 
 (menu-bar-mode nil)
 (tool-bar-mode nil)
+
+;; BASIC CUSTOMIZATION
+;; --------------------------------------
+
+(setq inhibit-startup-message t) ;; hide the startup message
+(load-theme 'material t) ;; load material theme
+; (global-linum-mode t) ;; enable line numbers globally
+
+(elpy-enable)
+(require 'py-autopep8)
+(add-hook 'elpy-mode-hook 'py-autopep8-enable-on-save)
+(add-hook 'python-mode-hook 'anaconda-mode)
+
+(add-to-list 'custom-theme-load-path "~/.emacs.d/themes")
