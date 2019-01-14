5076568b382e60f577a93d85c956d5bbc257b6ba
Duong Dang <duong.dang@hm.com>
2019-01-14 10:14:39 +0000

    Add snapshot.

--- dotemacs.el
+++ dotemacs.el
@@ -1,7 +1,7 @@
 (when (>= emacs-major-version 24)
   (require 'package)
   (add-to-list 'package-archives
-  	       '("melpa" . "http://melpa.org/packages/") t)
+	       '("melpa" . "http://melpa.org/packages/") t)
   (add-to-list 'package-archives
 	       '("melpa-stable" . "http://stable.melpa.org/packages/") t)
   (package-initialize))
@@ -15,12 +15,12 @@
 (unless package-archive-contents
   (package-refresh-contents))
 
-(setq package-list '(auto-complete ensime better-defaults material-theme elpy py-autopep8 color-theme anaconda-mode))
+(setq package-list '(auto-complete ensime better-defaults material-theme elpy py-autopep8 color-theme groovy-mode ws-butler fill-column-indicator flycheck magit))
 
 ; install the missing packages
 (dolist (package package-list)
   (unless (package-installed-p package)
-        (package-install package)))
+	(package-install package)))
 
 ;; auto-complete stuff
 (require 'auto-complete)
@@ -46,8 +46,6 @@
 (put 'narrow-to-region 'disabled nil)
 
 (add-to-list 'load-path "~/.emacs.d/lisp/")
-
-(add-hook 'before-save-hook 'delete-trailing-whitespace)
 
 (require 'ensime)
 (add-hook 'scala-mode-hook 'ensime-scala-mode-hook)
@@ -90,9 +88,70 @@
 (load-theme 'material t) ;; load material theme
 ; (global-linum-mode t) ;; enable line numbers globally
 
-(elpy-enable)
-(require 'py-autopep8)
-(add-hook 'elpy-mode-hook 'py-autopep8-enable-on-save)
-(add-hook 'python-mode-hook 'anaconda-mode)
+;; (elpy-enable)
+;; (require 'py-autopep8)
+;; (add-hook 'elpy-mode-hook 'py-autopep8-enable-on-save)
+;; (add-hook 'python-mode-hook 'anaconda-mode)
 
 (add-to-list 'custom-theme-load-path "~/.emacs.d/themes")
+
+(setq-default fill-column 100)
+
+(add-hook 'prog-mode-hook #'hs-minor-mode)
+
+(require 'ws-butler)
+(add-hook 'prog-mode-hook #'ws-butler-mode)
+
+(setq smerge-command-prefix "\C-cv")
+
+(global-set-key (kbd "C-x g") 'magit-status)
+
+(add-hook 'before-save-hook 'delete-trailing-whitespace)
+
+(defun xah-clean-empty-lines ()
+  "Replace repeated blank lines to just 1.
+Works on whole buffer or text selection, respects `narrow-to-region'.
+URL `http://ergoemacs.org/emacs/elisp_compact_empty_lines.html'
+Version 2017-09-22"
+  (interactive)
+  (let ($begin $end)
+    (if (region-active-p)
+	(setq $begin (region-beginning) $end (region-end))
+      (setq $begin (point-min) $end (point-max)))
+    (save-excursion
+      (save-restriction
+	(narrow-to-region $begin $end)
+	(progn
+	  (goto-char (point-min))
+	  (while (re-search-forward "\n\n\n+" nil "move")
+	    (replace-match "\n\n")))))))
+
+(require 'fill-column-indicator)
+(add-hook 'after-change-major-mode-hook 'fci-mode)
+
+(add-hook 'prog-mode-hook
+	  (lambda () (add-to-list 'write-file-functions 'delete-trailing-whitespace)))
+;; (add-hook 'prog-mode-hook
+;;  	  (lambda () (add-to-list 'write-file-functions 'xah-clean-empty-lines)))
+
+(require 'blacken)
+(defvar blacken-line-length 99)
+(add-hook 'python-mode-hook 'blacken-mode)
+
+(global-flycheck-mode)
+(column-number-mode 1)
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (magit yasnippet-snippets yaml-mode ws-butler pylint py-autopep8 material-theme groovy-mode flymake-python-pyflakes flymake-cursor flycheck fill-column-indicator ensime elpy dracula-theme color-theme better-defaults auto-complete anaconda-mode))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
